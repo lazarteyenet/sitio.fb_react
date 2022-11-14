995b51a4134de18833876a19a81025dd549086fb
lazarteyenet <olgaatamari17@gmail.com>
2022-11-14 01:36:33 -0600

    version 04 bf react

--- src/componente/firebase.js
+++ src/componente/firebase.js
@@ -3,10 +3,10 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    authDomain: 'yyyyyyyyyyyyyyyyy.firebaseapp.com',
-    projectId: 'yyyyyyyyyyyyyyyyy',
-    storageBucket: "yyyyyyyyyyyyyyyyy.appspot.com",
+    apiKey: 'AIzaSyB04HJV5wVLW-ZaX5nSY9P2G8cu5vTNuhA',
+    authDomain: 'fb-bdreact-8fd24.firebaseapp.com',
+    projectId: 'fb-bdreact-8fd24',
+    storageBucket: "fb-bdreact-8fd24.appspot.com",
 };
 
 const app = initializeApp(firebaseConfig);
